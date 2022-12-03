cc66d9b37f718e60e5bd2b5011a0a369d2feffeb
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-03 14:13:26 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,6 +1,3 @@
 function solution(my_string) {
-    const answer = my_string.split('').map((char) => 
-        char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()).join('');
-    
-    return answer;
+    return my_string.split('').map((v) => v === v.toLowerCase() ? v.toUpperCase() : v.toLowerCase()).join('');
 }
