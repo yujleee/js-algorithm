26ecb6b3f2afaecb497aa1df35b33a8fb5a436ac
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-03 14:30:03 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 42.14 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -6,19 +6,17 @@ const rl = readline.createInterface({
 
 let input = [];
 
-
-
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    const num = Number(input[0]);
     let str = '';
-    
-    for(let i=0; i<num; i++){
+    for(let i=0; i<Number(input[0]); i++){
         for(let j=0; j<=i; j++){
-            str+='*';
-        }   
-    str += '\n';
-}
-    console.log(str);   
+            str += '*';
+        }
+        
+        str += '\n';
+    }
+   
+    console.log(str);
 });
