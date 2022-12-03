14b59348d801b853d839d8a61be85f6fff5943c8
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-03 14:41:38 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,9 +1,5 @@
 function solution(my_string, num1, num2) {
-    // my_string을 split으로 쪼개기
-    // 구조분해할당으로 인덱스의 위치 바꾸고 join으로 결합
-    const str = my_string.split('');
-    [str[num1], str[num2]] = [str[num2], str[num1]];
-    
-    
-    return str.join('');
+    const answer = my_string.split('');
+    [answer[num1], answer[num2]] = [answer[num2], answer[num1]];
+    return answer.join('');
 }
