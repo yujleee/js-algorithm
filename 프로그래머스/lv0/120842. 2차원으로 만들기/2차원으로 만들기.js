4332f05d4084cedd29f21822353377ed35a60b55
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-06 09:44:56 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -2,8 +2,7 @@ function solution(num_list, n) {
     var answer = [];
     
     for(let i=0; i<num_list.length/n; i++){
-        let arr = [...num_list].slice(i * n, i * n + n);
-        answer.push(arr);
+        answer.push([...num_list].slice(i * n, i * n + n));
     }
   
     
