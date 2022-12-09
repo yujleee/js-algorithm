70e3b6ac6be99ce68e8dd0fa884fcb352d276181
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-09 11:00:25 +0900

    [level 1] Title: 예산, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,18 +1,5 @@
 function solution(d, budget) {
-    let answer = 0;
+    var answer = d.sort((a,b) => a - b).reduce((acc, curr) => acc + ((budget -= curr) >= 0), 0);
     
-    d.sort((a,b) => a-b);
-    
-     for (let i = 0; i < d.length; i++) {
-        budget -= d[i];
-        answer += 1;
-        
-        if (budget < 0){
-           answer--;
-           break;  
-        } 
-     
-    }
-      
     return answer;
 }
