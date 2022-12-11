2dcceef73a99c3182e8e4eee723f7cec6edb0ca1
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-11 15:13:05 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,5 +1,3 @@
 function solution(absolutes, signs) {
-    // signs의 값이 true -> 1 / false -> -1 곱하기
-    
-    return absolutes.reduce((acc, curr, idx) => acc + (curr * (signs[idx] ? 1 : -1)), 0);
+    return absolutes.reduce((acc, curr, idx) => acc + (curr * (signs[idx] ? 1 : -1)),0);
 }
