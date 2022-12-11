d8e3a5daefa163adc12e7bff713d814c9395fe75
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-11 15:43:13 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,5 +1,6 @@
 function solution(emergency) {
-    // 내림차순 정렬
-    const sorted = [...emergency].sort((a,b) => b-a);
-    return emergency.map((v) => (sorted.indexOf(v)+1));
+    // 높은 순서대로 정렬
+    // 값이 같으면 인덱스 push
+    const sort = [...emergency].sort((a,b) => b - a);
+    return emergency.map((item) => sort.indexOf(item) + 1);
 }
