860edf190cb1d8f0953ae972950f1586ebb2ce67
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-11 15:35:22 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,16 +1,7 @@
 function solution(s) {
-    // indexOf, lastIndexOf 일치하면 뽑기 -> sort
-    
-    let answer = '';
-    
-    for(let i=0; i<s.length; i++){
-        answer += s.indexOf(s[i]) === s.lastIndexOf(s[i]) ? s[i] : '';
-    }
-  
-    
-    return answer.split('').sort().join('');
+    var answer = [];
+    s.split('').forEach((char) => {
+        s.indexOf(char) === s.lastIndexOf(char) ? answer.push(char) : null;
+    })
+    return answer.sort().join('');
 }
-
-
-
-
