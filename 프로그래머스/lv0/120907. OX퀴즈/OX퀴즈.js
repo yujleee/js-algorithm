61f3cdbb7a9c62e2699831575e4c9f76517b1412
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-12 09:49:37 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,10 +1,21 @@
+// function solution(quiz) {
+//     var answer = [];
+    
+//    for(let i=0; i<quiz.length; i++) {
+//         const q = quiz[i].split(' ');
+//         +q[0] + (q[1] === '-' ? -1 : 1) * +q[2] === +q[4] ? answer.push('O') : answer.push('X');
+//    };
+    
+//     return answer;
+// }
+
 function solution(quiz) {
     var answer = [];
-    
-    for(let i=0; i<quiz.length; i++){
-        const q = quiz[i].split(' ');
-        Number(q[0]) + (q[1] === '-' ? -1 : 1) * Number(q[2]) === Number(q[4]) ? answer.push('O') : answer.push('X');
-    }
-    
-    return answer;
+    return quiz.map(t => {
+        const [calc, result] = t.split(' = ');
+        const sign = calc.includes('+') ? 1 : -1
+        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
+
+        return +a + (+b * sign) === +result ? 'O' : 'X'
+    });
 }
