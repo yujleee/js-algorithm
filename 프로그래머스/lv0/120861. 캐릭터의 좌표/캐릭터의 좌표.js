fc592f394b650ddb228b6e4859b0bbe050058397
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-13 10:31:11 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,22 +1,23 @@
 function solution(keyinput, board) {
-    var answer = [0,0];
+    let result = [0,0];
     
     const keys = {
-        up: [0, 1],
+        up: [0,1],
         down: [0, -1],
         left: [-1, 0],
         right: [1,0],
     };
     
-    keyinput.forEach((key) => {
-        if(Math.abs(answer[0] + keys[key][0]) <= Math.floor(board[0]/2) && 
-           Math.abs(answer[1] + keys[key][1]) <= Math.floor(board[1]/2)){
-            answer[0] += keys[key][0];
-            answer[1] += keys[key][1];
-        }
-        
-    });
+    for(let i=0; i<keyinput.length; i++){
+        const current = keys[keyinput[i]];
     
-    return answer;
-        
+        if(Math.abs(result[0]+current[0]) <= Math.floor(board[0]/2) && 
+           Math.abs(result[1]+current[1]) <= Math.floor(board[1]/2)){
+            result[0] += current[0];
+            result[1] += current[1];
+        }        
+               
+    }
+    
+    return result;
 }
