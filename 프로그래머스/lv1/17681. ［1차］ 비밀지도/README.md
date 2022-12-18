87b0582be86c246474fbc380f45a285e30fb4a41
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-18 20:15:37 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.12 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,22 +1,14 @@
 function solution(n, arr1, arr2) {
-    // arr1 , arr2 돌면서 2진수로 바꿔서 1을 #로 치환
-    let answer = [];
+    var answer = [];
     
-     for(let i=0; i<arr1.length; i++){
-            // 비트 OR 연산자로 1일때 다 합쳐주기
-            let binaryNum = (arr1[i] | arr2[i]).toString(2).split('');
-           
-            // 한 자리수일 경우 변환한게 n의 길이보다 짧으니 앞에 0을 채워줌
-            while(binaryNum.length < n){
-                binaryNum.unshift('0');
-            }
-         
-            const decodeStr = binaryNum.map((el) => el === '1' ? '#' : ' ').join('');
-            answer.push(decodeStr);
-            
-        }
-    
-
+    for(let i=0; i<arr1.length; i++){
+        let binary = (arr1[i] | arr2[i]).toString(2);
+        
+        binary = binary.padStart(n, '0').split('');
+        
+        let decode = binary.map((el) => el === '0' ? ' ' : '#').join('');
+        answer.push(decode);
+    }
     
     return answer;
 }
