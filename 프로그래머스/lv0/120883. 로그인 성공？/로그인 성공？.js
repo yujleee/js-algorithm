f021c1d59494b007924e96fe43da628f699512ae
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2022-12-18 19:59:23 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,6 +1,12 @@
 function solution(id_pw, db) {
     const [id, pw] = id_pw;
     
-    if(!db.find(([db_id]) => db_id === id)) return 'fail';
-    return db.find(([db_id, db_pw]) => db_id === id && db_pw === pw) ? 'login' : 'wrong pw';
+    for(let i=0; i<db.length; i++){
+        let [db_id, db_pw] = db[i];
+        
+        if(db_id === id && db_pw === pw) return 'login';
+        else if (db_id === id && db_pw !== pw) return 'wrong pw';
+    }
+    
+    return 'fail';
 }
