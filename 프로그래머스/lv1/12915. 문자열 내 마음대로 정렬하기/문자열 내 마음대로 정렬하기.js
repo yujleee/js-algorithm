8b7a236d48971d4827a08bfe9076a23c44e78b28
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2023-04-02 20:34:13 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -5,9 +5,9 @@ function solution(strings, n) {
             return 1;
         } else if(a[n] < b[n]){
             return -1;
-        } else {
-            return (a > b ? 1 : a < b ? -1 : 0);
         }
+        
+        return (a > b ? 1 : a < b ? -1 : 0);
     });
     
 }
