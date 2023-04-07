9ec35426206805c732337fb14a5daf9d294b3d31
Yujeong Lee <82587107+yujleee@users.noreply.github.com>
2023-04-07 20:44:59 +0900

    [level 2] Title: 올바른 괄호, Time: 3.37 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,13 +1,14 @@
 function solution(s){
     // 괄호가 ( 일때만 스택에 넣기
-    let stack = [];
+   let stack = [];
     
-    if(s.length <= 1 || s[0] === ')') return false;
+    if(s.length <= 1) return false;
+    if(s[0] === ')') return false;
     
     for(let i=0; i<s.length; i++){
-        s[i] === '(' ? stack.push(s[i]) : stack.pop();
+        s[i] === '(' ? stack.push('(') : stack.pop();
     }
     
-    return stack.length === 0 ? true : false;
+    return stack.length >= 1 ? false: true;
     
 }
